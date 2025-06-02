a1b9f4ac700f95f355979c223b3a1beb2f9489b4
Soneczko <soneczko@MacBook-Air-Daria.local>
2025-06-02 14:25:15 +0200

    Initial commit

---
+++ src/test/java/com/ztpai/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztpai.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
